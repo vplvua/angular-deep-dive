f3ff67b03595fa909e72ebfc925f0ec11459dfdd
Vasyl Pashko <vasyl.pashko@gmail.com>
2024-09-26 08:07:46 +0300

    add http service and async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,31 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
-import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { HighlightedDirective } from "./directives/highlighted.directive";
+import { Observable } from "rxjs";
+import { HttpClient, HttpParams } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  courses$: Observable<Course[]>;
 
-
-  courses = COURSES;
-
-  constructor() {
-
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    const params = new HttpParams().set("page", "1").set("pageSize", "10");
+
+    this.courses$ = this.http.get<Course[]>("/api/courses", { params });
   }
-
-
-
 }
